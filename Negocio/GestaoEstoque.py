c0a394289897871b86e0e83a5c2d3ddbbd31ceb4
LuisFRL <35008338+LuisFRL@users.noreply.github.com>
2018-01-10 16:56:04 -0200

    Update GestaoEstoque.py

--- Negocio/GestaoEstoque.py
+++ Negocio/GestaoEstoque.py
@@ -1,28 +1,31 @@
 from Entidades.Roupa import *
 from dados.RepositorioEstoque import *
 from dados.DOM.DOMEstoque import *
+from Negocio.Excecoes import *
 
 class GestaoEstoque:
 
     def __init__:
         self.repEstoque = RepositorioEstoque(DOMEstoque())
 
-    def CadastrarRoupaEstoque(self, roupa, quantidade):
-        self.repEstoque.CadastrarRoupa(roupa, quantidade)
+    def CadastrarRoupaEstoque(self, roupa, tipo, estilo, quantidade):
+
+        if not isinstance(roupa, Roupa):
+            raise TipoInvalidoException("Nao eh uma roupa")
+        
+        return self.repEstoque.CadastrarRoupa(roupa, tipo, estilo, quantidade)
         
     def ListarRoupas(self):
-        self.repEstoque.ListarRoupas()
+        return self.repEstoque.ListarRoupas()
 
     def ListarRoupasOferta(self):
-        self.repEstoque.ListarRoupasOferta()
+        return self.repEstoque.ListarRoupasOferta()
 
     def RecuperarRoupa(self, iD):
-        self.repEstoque.RecuperarRoupa(iD)
+        return self.repEstoque.RecuperarRoupa(iD)
 
     def RecuperarRoupa(self, iD):
-        self.repEstoque.RecuperarRoupaOferta(iD)
+        return self.repEstoque.RecuperarRoupaOferta(iD)
     
-    def CadastrarRoupasOferta(self, roupa, quantidade):
-        self.repEstoque.CadastrarRoupaOferta(roupa, quantidade)
-
-    def MaisVendidas(self):
+    def CadastrarRoupasOferta(self, roupa, tipo, estilo, quantidade):
+        return self.repEstoque.CadastrarRoupaOferta(roupa, tipo, estilo, quantidade)
